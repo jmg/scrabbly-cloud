a99b70124bfc124d6525ca07ea4f014f0317aec9
Juan Manuel García <jmg.utn@gmail.com>
2012-07-14 19:24:32 -0300

    playable board

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
 from juggernaut import Juggernaut
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.config.update(SECRET_KEY="fhdsjfhsdkjfhsk432853487")
+
+import simplejson as json
+from scrabbly import Dictionary, Board, Player, Tile, Word, InvalidPlayError
 
 
 @app.route("/", methods=['GET'])
 def index():
 
-    return render_template('index.html')
+    context = {}
+    context["letters"] = sorted([letter.decode("utf-8") for letter in Dictionary.letters.keys()])
+
+    size = (15, 15)
+    board = Board(size, [Player("jm")])
+    context["width"], context["height"] = size
+
+    session["board"] = board
+
+    return render_template('index.html', **context)
+
+
+@app.route("/board/play/", methods=['POST'])
+def play():
+
+    data = request.form["letters"]
+    tiles = json.loads(data)
+
+    word = Word([Tile(tile["letter"], (int(tile["x"]), int(tile["y"]))) for tile in tiles])
+    board = session["board"]
+
+    try:
+        board.play(word)
+        response = {"status": "ok"}
+    except InvalidPlayError, e:
+        response = {"status": "error", "message": unicode(e) }
+
+    return json.dumps(response)
 
 
 @app.route("/send_message", methods=['POST'])
@@ -20,6 +51,6 @@ def send_message():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
