706f04059c361a8ce23a91a6cd1b7e875265f297
Juan Manuel García <jmg.utn@gmail.com>
2012-07-20 01:58:24 -0300

    heroku app

--- app.py
+++ app.py
@@ -21,6 +21,5 @@ from admin import *
 from views import *
 
 if __name__ == '__main__':
-	
 	port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
