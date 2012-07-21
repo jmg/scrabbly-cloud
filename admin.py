964cf1fdba38bc88ae1bb1e2bde80bfbce45e491
Juan Manuel García <jmg.utn@gmail.com>
2012-07-21 12:01:04 -0300

    admin improves

--- admin.py
+++ admin.py
@@ -1,10 +1,31 @@
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqlamodel import ModelView
+from flask.ext import wtf
 
 from app import app
 from models import *
 
 admin = Admin(app)
-admin.add_view(ModelView(Board, db.session))
-admin.add_view(ModelView(Tile, db.session))
-admin.add_view(ModelView(Player, db.session))
+
+class BoardAdmin(ModelView):
+
+	form_args = dict(	
+        players=dict(validators=[wtf.Optional()]),
+        tiles=dict(validators=[wtf.Optional()])
+    )
+
+class PlayerAdmin(ModelView):
+
+	form_args = dict(
+        players=dict(validators=[wtf.Optional()])
+    )
+
+class TileAdmin(ModelView):
+
+	form_args = dict(
+        tiles=dict(validators=[wtf.Optional()])
+    )
+
+admin.add_view(BoardAdmin(Board, db.session))
+admin.add_view(PlayerAdmin(Player, db.session))
+admin.add_view(TileAdmin(Tile, db.session))
